da86f773f1926f24a4d33d37feabfa33f71b8684
unknown <nadim.dipu@northsouth.edu>
2021-07-14 15:17:49 +0600

    User Create API

---
+++ spring-rest-api/src/main/java/com/example/springrestapi/service/StudentService.java
@@ -0,0 +1,19 @@
+package com.example.springrestapi.service;
+
+import com.example.springrestapi.model.Student;
+import com.example.springrestapi.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+    @Autowired
+    private StudentRepository repository;
+
+    public List<Student> getStudents() {
+        return repository.findAll();
+    }
+
+}
